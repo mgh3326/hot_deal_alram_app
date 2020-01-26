2f68d8f297f773a92f84c1a08bcdb8f7663d3d6b
robin <mgh3326@naver.com>
2020-01-27 07:19:07 +0900

    update viewModel getText().observe

--- app/src/main/java/me/khmoon/hotdealalram/ui/share/ShareFragment.java
+++ app/src/main/java/me/khmoon/hotdealalram/ui/share/ShareFragment.java
@@ -5,11 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 import me.khmoon.hotdealalram.R;
 
 public class ShareFragment extends Fragment {
@@ -18,11 +18,10 @@ public class ShareFragment extends Fragment {
 
   public View onCreateView(@NonNull LayoutInflater inflater,
                            ViewGroup container, Bundle savedInstanceState) {
-    shareViewModel =
-            ViewModelProviders.of(this).get(ShareViewModel.class);
+    shareViewModel = new ViewModelProvider(this).get(ShareViewModel.class);
     View root = inflater.inflate(R.layout.fragment_share, container, false);
     final TextView textView = root.findViewById(R.id.text_share);
-    shareViewModel.getText().observe(this, new Observer<String>() {
+    shareViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
       @Override
       public void onChanged(@Nullable String s) {
         textView.setText(s);
--- app/src/main/java/me/khmoon/hotdealalram/ui/tools/ToolsFragment.java
+++ app/src/main/java/me/khmoon/hotdealalram/ui/tools/ToolsFragment.java
@@ -5,11 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 import me.khmoon.hotdealalram.R;
 
 public class ToolsFragment extends Fragment {
@@ -18,11 +18,10 @@ public class ToolsFragment extends Fragment {
 
   public View onCreateView(@NonNull LayoutInflater inflater,
                            ViewGroup container, Bundle savedInstanceState) {
-    toolsViewModel =
-            ViewModelProviders.of(this).get(ToolsViewModel.class);
+    toolsViewModel = new ViewModelProvider(this).get(ToolsViewModel.class);
     View root = inflater.inflate(R.layout.fragment_tools, container, false);
     final TextView textView = root.findViewById(R.id.text_tools);
-    toolsViewModel.getText().observe(this, new Observer<String>() {
+    toolsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
       @Override
       public void onChanged(@Nullable String s) {
         textView.setText(s);
